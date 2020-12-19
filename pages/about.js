42cc0cb39f541b22c321fcfd5f8c00518ad89d1f
ijmorales <moralesignaciojoaquin@gmail.com>
2020-12-19 20:33:29 -0300

    Extraccion del Layout general a un componente y creacion de pagina About

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from './layout/Layout'
+
+const About = () => {
+  return (
+    <Layout>
+      <h1 className="font-extrabold">
+        Acerca de
+      </h1>
+    </Layout>
+  )
+}
+
+export default About
