1213b69efd125d752a838d4ac63df56e4cfb5016
ijm <moralesignaciojoaquin@gmail.com>
2021-01-06 17:38:13 -0300

    Layout: CursoGrid

---
+++ components/cursos/CursoGrid.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+import CursoCard from './CursoCard'
+
+export default function CursoGrid ({ cursos }) {
+  return (
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 2xl:gap-9">
+      {cursos.map((curso, index) => (
+        <CursoCard key={index} curso={curso}/>
+      ))}
+    </div>
+  )
+}
+
+CursoGrid.propTypes = {
+  cursos: PropTypes.array
+}
