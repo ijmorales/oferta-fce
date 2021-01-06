8469ab5ef30ffba100a635ee69f6b47c44eda86d
ijm <moralesignaciojoaquin@gmail.com>
2021-01-06 17:37:22 -0300

    Refactor Card to make it more reusable

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,7 @@
 export default function Card({ children }) {
   return (
-    <article className="bg-white lg:w-96 w-93.5 h-59.5 shadow-md rounded-lg p-5 flex flex-col justify-between">
-      { children }
-    </article>
+    <div className="bg-white shadow-md rounded-lg">
+      {children}
+    </div>
   )
 }
