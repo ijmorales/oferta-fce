267c7a33cb9ced671869c4c03a019cd8a31445d2
ijm <moralesignaciojoaquin@gmail.com>
2021-02-17 19:33:26 -0300

    fix: Tailwind and Next export config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   corePlugins: {
     container: false,
   },
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
